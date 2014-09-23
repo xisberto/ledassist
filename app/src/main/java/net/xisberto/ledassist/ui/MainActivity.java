2ad4f99dc83e012b3c3c40a056d96c30fa6e0f64
Humberto Fraga <xisberto@gmail.com>
2014-09-22 23:03:07 -0300

    Removing unused menu

--- app/src/main/java/net/xisberto/ledassist/ui/MainActivity.java
+++ app/src/main/java/net/xisberto/ledassist/ui/MainActivity.java
@@ -4,10 +4,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.text.format.DateFormat;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -123,24 +121,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == android.R.id.home) {
             getSupportFragmentManager()
                     .popBackStack();
-            return true;
-        }
-        if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
