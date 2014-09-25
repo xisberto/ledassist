7619fb238718454a3bfe4993b70b1881b382e116
Humberto Fraga <xisberto@gmail.com>
2014-09-24 22:33:52 -0300

    Adding a boot receiver

---
+++ app/src/main/java/net/xisberto/ledassist/control/BootReceiver.java
@@ -0,0 +1,19 @@
+package net.xisberto.ledassist.control;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    public BootReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            if (Settings.isActive(context)) {
+                Scheduler.startSchedule(context);
+            }
+        }
+    }
+}
